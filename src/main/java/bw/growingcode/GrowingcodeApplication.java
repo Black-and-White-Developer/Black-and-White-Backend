057efa79d9ff68fd1e787880afe76ae32dbde32d
TaegeunYou <dbxorms2310@naver.com>
2024-09-28 17:18:46 +0900

    Initial commit

---
+++ src/main/java/bw/growingcode/GrowingcodeApplication.java
@@ -0,0 +1,13 @@
+package bw.growingcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowingcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowingcodeApplication.class, args);
+	}
+
+}
